ca05ccb6829697dbccc9149a702cc3f457e999f8
siqi <yisiqiyisiqi@gmail.com>
2020-06-24 16:08:38 +1000

    remove datum tf query from icp matcher pipeline

--- dataset_tools/src/run_pipeline.cpp
+++ dataset_tools/src/run_pipeline.cpp
@@ -41,7 +41,7 @@ RunPipeline::WaitForMessages(float time_out) {
     }
 
     if ((ros::Time::now() - start_time).toSec() > time_out) {
-        ROS_ERROR("wait for ICP matcher timed out");
+        ROS_ERROR("wait for pipeline messages timed out");
       break;
     }
 
